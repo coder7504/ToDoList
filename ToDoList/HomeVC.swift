6986a824af2e9c4ad88223d38dbeb2bfcb5c1c6b
Asliddin Mahmudov <devasliddin@gmail.com>
2022-11-20 18:21:27 +0500

    launch is added

--- ToDoList/HomeVC.swift
+++ ToDoList/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  ToDoList
 //
 //  Created by Asliddin Mahmudov on 20/11/22.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
-
 }
-
